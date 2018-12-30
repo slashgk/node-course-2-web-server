7b29aa55337c7399f68c1637d80a797f925cd22b
GAURAV KUMAR <gaurav.grd@gmail.com>
2018-12-30 17:42:35 +0530

    Added projects page

--- server.js
+++ server.js
@@ -55,6 +55,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'My portfolio'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         error_message: 'Error'
